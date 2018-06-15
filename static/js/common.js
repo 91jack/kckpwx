847dba4bc4729bbeb5c7ef96dabe1c0b657ed22d
91jack <2764132626@qq.com>
2018-06-15 17:55:40 +0800

    1
    1

--- static/js/common.js
+++ static/js/common.js
@@ -1,14 +1,14 @@
 // 移动端响应式匹配
 (function () {
-  document.addEventListener('DOMContentLoaded', function () {
-	var deviceWidth = document.documentElement.clientWidth;
-	console.log(deviceWidth)
-		if(deviceWidth < 375){
-			console.log(deviceWidth)
-			document.documentElement.style.fontSize = deviceWidth / 26.66 + 'px';
-		}else{
-			document.documentElement.style.fontSize = deviceWidth / 31.25 + 'px';
-		}
+	document.addEventListener('DOMContentLoaded', function () {
+		var deviceWidth = document.documentElement.clientWidth;
+		console.log(deviceWidth)
+			if(deviceWidth < 375){
+				console.log(deviceWidth)
+				document.documentElement.style.fontSize = deviceWidth / 26.66 + 'px';
+			}else{
+				document.documentElement.style.fontSize = deviceWidth / 31.25 + 'px';
+			}
 		
 		
 	   }, false);
@@ -27,24 +27,12 @@
 })();
 
 
+
+
 // 返回上一页
 $('.goback').on('click', function(){
 	window.history.back();
 })
 
-// jQueryAjax 封装
-function commonAjax(url,data){
-	$.ajax({
-	    type: "POST",
-	    url: url,
-	  	data: data,
-	    success: function(res){
-			if(res.status == 2000){
-				return res;
-			}else{
-				console.log(res)
-			}
-	    }
-	})	
-}
+
 
