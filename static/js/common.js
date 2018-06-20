741a1aad396b004968d7e822b1603c40aa13fcf3
91jack <2764132626@qq.com>
2018-06-20 20:15:45 +0800

    1
    1

--- static/js/common.js
+++ static/js/common.js
@@ -2,32 +2,14 @@
 (function () {
 	document.addEventListener('DOMContentLoaded', function () {
 		var deviceWidth = document.documentElement.clientWidth;
-		//console.log(deviceWidth)
-//			if(deviceWidth < 375){
-//				//console.log(deviceWidth)
-//				document.documentElement.style.fontSize = deviceWidth / 26.66 + 'px';
-//			}else{
-				document.documentElement.style.fontSize = deviceWidth / 31.25 + 'px';
-//			}
-		alert(deviceWidth)
-		
+			document.documentElement.style.fontSize = deviceWidth / 31.25 + 'px';
 	   }, false);
 	   
 	window.onresize = function(){
 		var deviceWidth = document.documentElement.clientWidth;
-		
-//		if(deviceWidth < 375){
-//			//console.log(deviceWidth)
-//			document.documentElement.style.fontSize = deviceWidth / 26.66 + 'px';
-//		}else{
-			document.documentElement.style.fontSize = deviceWidth / 31.25 + 'px';
-		//}
-		console.log(desinWidth)
+		document.documentElement.style.fontSize = deviceWidth / 31.25 + 'px';
 	};
 })();
-
-
-
 
 // 返回上一页
 $('.goback').on('click', function(){
